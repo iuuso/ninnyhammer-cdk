2d375a69136d8a376b59f5d7d76b0500b4768187
Juuso Karlström <iuuso@Sherlock.lan>
2021-03-11 23:41:28 +0200

    Added Cloudfront distribution and version upgrade

--- lib/ninnyhammer-cdk-stack.ts
+++ lib/ninnyhammer-cdk-stack.ts
@@ -1,4 +1,6 @@
 import * as s3 from '@aws-cdk/aws-s3';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 import { CfnOutput, Construct, Stage, Stack, StageProps, StackProps } from '@aws-cdk/core';
 
 export class WebLayerStack extends Stack {
@@ -7,6 +9,12 @@ export class WebLayerStack extends Stack {
     
     const webbucket = new s3.Bucket(this, 'NinnyhammerBucket', {
       bucketName: 'ninnyhammer-web-bucket',
+    });
+
+    new cloudfront.Distribution(this, 'NinnyhammerCF', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(webbucket)
+      },
     });
 };
 }
